59df00198846ed4b7718a0bd87c4faf6bb046460
Dmitry Korolev <dmitrykorolev@Dmitrys-MacBook-Pro.local>
2024-02-25 13:10:07 +0300

    nothing

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-# from fastapi.responses import HTMLResponse
-# from draw import draw, add_bounding_boxes
+
 from computer_vision.classification import get_clf_prediction
-# from computer_vision.detection import get_bbox_prediction
-# from computer_vision.emotions import get_bbox_prediction_tf
+
 from PIL import Image
 import numpy as np
 import pybase64
@@ -47,39 +45,13 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             data = await websocket.receive()
-            # data = json.loads(data)
-            # imageByt64 = data['data']['image'].split(',')[1]
             _, raw = data.get("text").split(",")
 
 
             img = Image.open(io.BytesIO(base64.b64decode(raw)))
             clf_prediction = get_clf_prediction(img)
 
-            # img = Image.open(io.BytesIO(base64.b64decode(imageByt64)))
-            # yolo_prediction = get_bbox_prediction(img)
-
-            # nparr = np.fromstring(base64.b64decode(imageByt64), np.uint8)
-            # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-            # emotions_predictions = get_bbox_prediction_tf(img)
-
-            # with open("computer_vision/imageToSave.png", "wb") as fh:
-            #     fh.write(pybase64.b64decode((raw)))
-            # print(type(img))
-            # result = draw(img)
-            # result = Image.fromarray(result)
-            #
-            # result = add_bounding_boxes(img)
-            # result = base64.b64encode(result)
-            # print(result)
-
-            # imgByteArr = io.BytesIO()
-            # result.save(imgByteArr, format="PNG")
-            # imgByteArr = base64.b64encode(imgByteArr.getvalue())
-            # print(imgByteArr)
-
-            # print((base64.b64encode(result)))
-            # result = result()
-            # print(raw)
+           
 
             await manager.send_json(clf_prediction, websocket)
     except WebSocketDisconnect:
